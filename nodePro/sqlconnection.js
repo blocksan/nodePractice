414ca1e9b4e54e77c40c1fa64c0763e3f2a64a2f
sandeep_ghosh <sandyghosh555@gmail.com>
2016-02-17 10:44:54 +0530

    CRUD with NodeJs only Create, Read and Update

---
+++ nodePro/sqlconnection.js
@@ -0,0 +1,53 @@
+//performing crud operations  CREATE READ UPDATE DELETE
+
+var mysql =require('mysql');
+var con = mysql.createConnection({
+	host:"localhost",
+	user:"root",
+	password:"",
+	database:"nodedata"
+});
+
+//connecting to the database
+
+con.connect(function(err){
+	if(err){
+	console.log("error in connection");
+	return;
+	}	
+console.log('connection established');
+});
+
+//executing the query for fetching the data
+
+con.query('SELECT * FROM nodetable',function(err,rows)
+{
+  if(err) throw err;
+  		console.log('Data received from Db:\n');
+  	    console.log('Sno   '+' Name\n');
+	  for(var i=0;i<rows.length;i++)
+	  {
+	  	console.log(rows[i].sno+' ==> '+rows[i].name);
+	  };
+
+  //console.log(rows);   //for showing the resultant output of the database in the json format 
+});
+
+//query for inserting into the database 
+var query={sno:'',name:'indu'};
+con.query('INSERT INTO nodetable SET ?',query,function(err,res)
+{
+	if(err) throw err;
+	console.log('last effected rows for insertion  '+res.affectedRows);  // used to show the last affected row
+});
+
+//query to update the database
+con.query('UPDATE nodetable SET name = ? WHERE sno = ?',['pooja',2],function(err,result)
+{
+	if(err) throw err;
+	console.log(' last effected row for updation   '+result.affectedRows);
+});
+
+
+//ending the connection
+con.end();
