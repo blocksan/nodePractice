fa8ec6cf83cab5cad46ffc1a0e2c133100ec49fc
sandeep_ghosh <sandyghosh555@gmail.com>
2016-02-17 16:12:20 +0530

    Form handling using NodeJs -- formsubmit.js and index.html

---
+++ nodePro/formsubmit.js
@@ -0,0 +1,63 @@
+var mysql =require('mysql');
+var fs=require('fs');
+var http=require('http');
+var formidable = require("formidable");
+var util = require('util');
+var mysql =require('mysql');
+var con = mysql.createConnection({
+	host:"localhost",
+	user:"root",
+	password:"",
+	database:"nodedata"
+});
+
+var server=http.createServer(function(req,res){
+	
+	if (req.method.toLowerCase() == 'get') {
+        displayForm(res);
+    } else if (req.method.toLowerCase() == 'post') {
+        processForm(req, res);
+    }
+});
+
+//function to display form
+function displayForm(res)
+{	
+	fs.readFile('nodePro/index.html',function(err,data){
+		res.writeHead(200,
+			{'Content-Type':'text/html',
+			'Content-Length':data.length
+			});
+		res.write(data);
+		res.end();
+	});
+}
+function processForm(req,res)
+{
+	var form=new formidable.IncomingForm();
+
+		form.parse(req,function(err,fields,files,insertFun)
+		{
+			res.writeHead(200,{'Content-Type':'text/plain'});
+			res.write("recieved the data \n");
+			res.end(util.inspect({
+				fields:fields,
+				files:files,
+				insertFun:insertIntoData(fields)
+			}));
+			
+			
+		})
+}
+function insertIntoData(fields){
+var query={sno:'',name:fields.name};
+con.query('INSERT INTO nodetable SET ?',query,function(err,res)
+{
+	if(err) throw err;
+	console.log('last effected rows for insertion  '+res.affectedRows);  // used to show the last affected row
+});
+}
+
+
+server.listen(3000);
+console.log('server listening');
