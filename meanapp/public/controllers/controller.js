c436dabfef5f74df70a1ed06341ab3e0ea14f5e3
sandeep_ghosh <sandyghosh555@gmail.com>
2016-02-22 23:58:19 +0530

    just made the indentation better of the js files

--- meanapp/public/controllers/controller.js
+++ meanapp/public/controllers/controller.js
@@ -1,37 +1,46 @@
 var app=angular.module('app',[]);
 	app.controller('studentController', function($scope,$http) {  //using http module to send a request to server
 		
+		//refresh function
 		var refresh=function()
-		{	        			//making the refresh function so that it can be called whenever new data is inserted or updated to change the front-view data
-	 
-			$http.get('/contactlist').success(function(response){    //sending the route url to node server to request data and data received in json format
-			  //	console.log('data received from the server');   //just consoling just to check 
-				$scope.person=response;        //storing the received data into person array
-				$scope.contact="";           //this is use to set input boxes to blank
+		{	        											  	//making the refresh function so that it can be called whenever new data is inserted or updated to change the front-view data
+			$http.get('/contactlist').success(function(response){   //sending the route url to node server to request data and data received in json format
+			  //console.log('data received from the server');       //just consoling just to check 
+				$scope.person=response;      				        //storing the received data into person array
+				$scope.contact="";        						   //this is use to set input boxes to blank
 			});
 		};
-		refresh();              //calling the refresh function onload to load the data
+		refresh();           									   //calling the refresh function onload to load the data
+	
+	//addcontact function 
 		$scope.addContact=function(){
 			//console.log($scope.contact);
-			$http.post('/contactlist',$scope.contact).success(function(response){   //using http.post module to post data to the node server
-				/*console.log(response);*/  //viewing the newly inserted result
+			$http.post('/contactlist',$scope.contact).success(function(response)
+			{  														 //using http.post module to post data to the node server
+				/*console.log(response);*/  						//viewing the newly inserted result
 				refresh();
 			})
 		};
+
+	//removecontact function
 		$scope.removeMe=function(id){
-		//	console.log(id);
-			$http.delete('/contactlist/'+id).success(function(response){   //using http.post module to post data to the node server
-				/*console.log(response);*/  //viewing the newly inserted result
+			//console.log(id);
+			$http.delete('/contactlist/'+id).success(function(response){  
+																	 //using http.post module to post data to the node server
+				/*console.log(response);*/  						//viewing the newly inserted result
 				refresh();
 			});
 		};
+
+	//editcontact function
 		$scope.editMe=function(id){
 			console.log(id);
-			/*$http.post('/contactlist',id).success(function(response){   //using http.post module to post data to the node server
-				/*console.log(response);*/  //viewing the newly inserted result
+			/*$http.post('/contactlist',id).success(function(response)
+			{  														 //using http.post module to post data to the node server
+				/*console.log(response);*/  						//viewing the newly inserted result
 				/*refresh();
 			})*/
 		};
 		
-	});
+});   																//end of controller function
 	   
--- meanapp/server.js
+++ meanapp/server.js
@@ -5,20 +5,21 @@ var bodyparser=require('body-parser');
 
 var db=mongojs('contactlist',['contactlist']);
 
-app.use(express.static(__dirname+"/public"));  //for giving the common path for html,css static pages from express module
-app.use(bodyparser.json());                    //for parsing the JSON body of input boxes from the client side
+app.use(express.static(__dirname+"/public"));     //for giving the common path for html,css static pages from express module
+app.use(bodyparser.json());                       //for parsing the JSON body of input boxes from the client side
 	
-	//	console.log(__dirname);					//for printing the current directory path 
+	//	console.log(__dirname);					 //for printing the current directory path 
 
-		app.get('/contactlist',function(err,res){   //receiving request from the client side using angular
-		console.log("request received from the client"); //consoling the request just to check
+app.get('/contactlist',function(err,res){        //receiving request from the client side using angular
+
+console.log("request received from the client"); //consoling the request just to check
 			
-		db.contactlist.find(function(errcon,docs){   //docs is the data sent from the mongodb server
+db.contactlist.find(function(errcon,docs){       //docs is the data sent from the mongodb server
 
-			console.log(docs); 
-			res.json(docs);
+	console.log(docs); 
+	res.json(docs);
 
-		});
+});
 
 		/*person= [{
 		               name: "sandeep",
@@ -30,23 +31,30 @@ app.use(bodyparser.json());                    //for parsing the JSON body of in
 		               email: "tush@gmail.com",
 		               phone:"87687687"
 		            }
-		             ];*/          //dummy data just for checking
-		//var contactlist=person;    //storing person array in another variable
-		//res.json(contactlist);		// converting contactlist array into json array type data so that it can be readable by the angular controller
+		             ];*/         			  //dummy data just for checking
+		//var contactlist=person;  			  //storing person array in another variable
+		//res.json(contactlist);		     // converting contactlist array into json array type data so that it can be readable by the angular controller
 
-		});
+});
 
+	//insert module into mongodb
 		app.post('/contactlist',function(req,res){   			 //app.post method to use the post data from the angular client side
 		//	console.log(req.body);              				//req.body is the data received from the client in JSON format and to parse its body we use bodyParser module
 			db.contactlist.insert(req.body,function(err,doc){   //insert function for mongodb
 				res.json(doc);  
 			});
 		});
+
+	//delete module into mondodb
 		app.delete('/contactlist/:id',function(req,res){
-			var id = req.params.id;
+			var id = req.params.id;								//id is get from the URL and parsed as params.id 
 			db.contactlist.remove({_id:mongojs.ObjectId(id)},function(err,doc){
 				res.json(doc);
 			});
+		});
+
+		app.update('/contactlist/:id',function(req,res){
+			var id=req.params.id;
 		});
 
 		 /*app.get('/',function(req,res){
